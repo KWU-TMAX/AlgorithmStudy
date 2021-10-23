2b24ae7bc1df8e78872f54065759ab0fe0b62f3b
sanghyeop_jeon <wjstkdguq92@naver.com>
2021-10-23 23:32:49 +0900

    [chore] 프로그래머스 - 주식가격 (주석 추가)

--- SHJ/주식가격/main.cpp
+++ SHJ/주식가격/main.cpp
@@ -9,15 +9,18 @@ vector<int> answer;
 vector<int> solution(vector<int> prices) {
     for(int i =0 ; i<prices.size(); i++) {
         int a = 0;
+        // 현재 가격을 끝까지 순회
         for(int j = i+1 ; j<prices.size(); j++)
         {
             a++;
+            // 현재 가격보다 큰 값을 만나면 break;
             if(prices[i] > prices[j]) {
                 break;
             }
         }
         answer.push_back(a);
     }
+    // 마지막은 무조건 0
     answer[answer.size()] = 0;
     return answer;
 }
