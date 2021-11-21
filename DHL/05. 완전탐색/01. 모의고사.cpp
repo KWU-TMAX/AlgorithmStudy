3b328bb9d13d2dfe21e3823801738f4037f5570a
ssu2030 <31645582+ssu2030@users.noreply.github.com>
2021-11-21 23:24:54 +0900

    Update 01. 모의고사.cpp

--- DHL/05. 완전탐색/01. 모의고사.cpp
+++ DHL/05. 완전탐색/01. 모의고사.cpp
@@ -11,21 +11,20 @@ vector<int> solution(vector<int> answers) {
 	int firstStudent[5] = { 1, 2, 3, 4, 5 };
 	int secondStudent[8] = { 2, 1, 2, 3, 2, 4, 2, 5 };
 	int thirdStudent[10] = { 3, 3, 1, 1, 2, 2, 4, 4, 5, 5 };
+	// 각각 학생들의 규칙들
 	int score[3] = {0,0,0};
-    int maxScore = 0;
-    
+	int maxScore = 0;
 	for (int i = 0; i < answers.size(); i++) {
-    // 찍신 강림했을때 각 score배열의 원소를 ++ 한다. 
+        	// 찍신 강림했을때 각 score배열의 원소를 ++ 한다. 
 		if (firstStudent[i % 5] == answers[i]) score[0]++;
 		if (secondStudent[i % 8] == answers[i]) score[1]++;
 		if (thirdStudent[i % 10] == answers[i]) score[2]++;
 	}
-    maxScore = max(max(score[0],score[1]),score[2]);
-    // 점수의 최댓값을 찾는다. 
-    for(int i = 0; i < 3; i++) {
-        // 최댓값을 가지는 모두를 push_back한다. 
-        if(maxScore == score[i]) answer.push_back(i+1);       
-    }
-    
+    	maxScore = max(max(score[0],score[1]),score[2]);
+    	// 점수의 최댓값을 찾는다. 
+    	for(int i = 0; i < 3; i++) {
+        	// 최댓값을 가지는 모두를 push_back한다. 
+        	if(maxScore == score[i]) answer.push_back(i+1);       
+    	}
 	return answer;
 }
