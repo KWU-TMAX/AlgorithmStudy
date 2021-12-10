f431d801c3354570a92e2dc0aaf41e664a7e52f6
shyoup <46154001+shyoup@users.noreply.github.com>
2021-12-10 10:06:40 +0900

    주석 업데이트

--- SHJ/조이스틱/solution.js
+++ SHJ/조이스틱/solution.js
@@ -11,15 +11,15 @@ function solution(name) {
     for(let i=0; i<name.length; i++) {
         if(name[i] !== 'A') notA.push(i);
     }
-    let min = notA[notA.length -1];
-    for(let j=notA.length-1; j>=0; j--) {
-        let left = notA[j-1] ? notA[j-1] * 2 : 0;
-        let right = name.length - notA[j];
+    let min = notA[notA.length -1];    // 오른쪽으로 다이렉트로 이동했을 때 ->입력하는 횟수
+    for(let j=notA.length-1; j>=0; j--) {    // 여기 for문은 되돌아갔을 때 좌우 키 입력하는 횟수 계산
+        let left = notA[j-1] ? notA[j-1] * 2 : 0;   // A가 아닌 j번째 index로 갈때 j-1까지 ->하다가 <-하고
+        let right = name.length - notA[j];          // <-해서 j번째 index로 도착하는 거리 계산
         if(left+right < min) {
-            min = left+right;
+            min = left+right;                   // 총 거리 계산했을 때 다이렉트로 간 거리보다 적으면 min값 변경
         }
     }
-    return answer + min;
+    return answer + min;            // 위 아래 움직인 횟수 + 좌우 움직인 횟수
 }
 
 console.log(solution("JEROEN"));     // 56
