beb997f2ef9f3678539e90b092c191305d60a53b
ssu2030 <31645582+ssu2030@users.noreply.github.com>
2021-12-12 00:24:50 +0900

    Update 03. 큰수 만들기.cpp

--- DHL/06. 탐욕법/03. 큰수 만들기.cpp
+++ DHL/06. 탐욕법/03. 큰수 만들기.cpp
@@ -1,34 +1,32 @@
-#include <string>
-#include <vector>
-#include <algorithm>
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-string solution(string number, int k) {
+string solution(string name, int k)
+{
     string answer = "";
-    vector<int> numVector;
-    // 문자열을 숫자로 만들어서 벡터에 넣는다.
-    for(int i = 0; i < number.size(); i++) {
-        int tmp = 0;
-        tmp= number[i] - '0';
-        numVector.push_back(tmp);
+    int position = 0;
+    int maxPosition = 0;
+    char maxNum = name[0];
+    int numLength = name.length()-k;
+    
+    for (int i = 0; i < numLength; i++)
+    {
+        for (int j = position + 1; j <= k + position; j++)
+        {
+            if (name[maxPosition] < name[j])
+            {
+                maxNum = name[j];
+                maxPosition = j;
+            } 
+        }
+        // 뺄 수 있는 위치만큼 보고, 그중 가장 큰 수를 넣는다
+        k -= maxPosition-position;
+        // max position 만큼 뺐으니까, 뺄수있는 수도 적어져야한다.
+        answer += name[maxPosition];
+        position = maxPosition+1;
+        maxPosition = position;
     }
-    
-    // 람다를 활용하여 내림 차순으로 sorting
-    sort(numVector.begin(),numVector.end(),[](int a, int b) {	
-			return a > b;
-		});
-    
-    // k만큼 pop
-    for(int i = 0; i < k; i++) {
-        numVector.pop_back();
-    }
-    
-		// 남은수 그대로 이어 붙이면 최대값
-    for(int i=0; i < numVector.size();i++){
-        answer += to_string(numVector[i]);
-    }
-    
     return answer;
 }
