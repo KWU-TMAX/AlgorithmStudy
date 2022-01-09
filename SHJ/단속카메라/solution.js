248200cfefaa3ff30edfbbb188033c9181537079
sanghyeop_jeon <wjstkdguq92@naver.com>
2022-01-09 23:40:35 +0900

    [feat] 프로그래머스 - 단속 카메라 중간커밋

---
+++ SHJ/단속카메라/solution.js
@@ -0,0 +1,48 @@
+function solution(routes) {
+    let array = [];
+
+    function check(route) {
+        let ret = false;
+        if(array.length === 0) {
+            array.push(route);
+            return;
+        }
+        for(let i=0; i<array.length; i++) {
+            if(array[i][0] <= route[0] && array[i][1] >= route[0]) {
+                ret = true;
+                array[i][0] = route[0];
+            }
+            if(array[i][0] <= route[1] && array[i][1] >= route[1]) {
+                ret = true;
+                array[i][1] = route[1];
+            }
+            if(ret) {
+                return;
+            }
+        }
+        array.push(route);
+    }
+
+    routes.sort((a,b) => b[0] - a[0]);
+    for(const route of routes) {
+        check(route);
+    }
+    return array.length;
+}
+
+// solution([[-20,-15], [-14,-5], [-18,-13], [-5,-3]]);
+console.log(solution([[1,15], [12,15], [16,17], [16,18], [3, 16], [9,11]]));
+
+/* https://mungto.tistory.com/49
+1. 정렬을 합니다. 기준점은 차량이 진입하는 순간입니다.
+정렬을 하는 이유는 순서대로 접근하기 쉽게하기 위해서입니다.
+2. 처음 차가 나가는 거리를 임시변수에 넣어줍니다.
+이 값을 이용해서 비교를 합니다.
+3. 다음차가 현재차가 나간후에 들어온다면 카메라를 추가해야합니다.
+나가는 순간에 카메라를 설치해도 다음차를 찍지못하기 때문입니다.
+4. 현재차보다 다음차가 나가는 거리가 짧다면 변수값을 갱신해줍니다.
+현재 차보다 늦게 들어왔지만 더 빨리 나가는 차가 있을때는 둘이 겹치는 부분에
+카메라를 두기 위해서 입니다.
+5. 위 과정을 반복하면 최종 카메라 -1개가 나옵니다.
+마지막 카메라가 추가되지 않기 때문입니다.
+따라서 시작할때 카메라 대수를 1대로 설정하거나 반환전에 +1을 해서 리턴합니다. */
